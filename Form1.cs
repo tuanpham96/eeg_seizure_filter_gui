e6bfd84250faf43081479f375b7290d858a24e81
Tuan Pham <tuanph18@uchicago.edu>
2018-10-18 10:24:46 -0500

    separated prompt into a class and made a param class
    for easier management

--- Form1.cs
+++ Form1.cs
@@ -4,18 +4,11 @@ using System.Threading;
 using System.Windows.Forms;
 using System.IO;
 using System.Net.Sockets;
-
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Diagnostics;
 
 using LiveCharts;
 using LiveCharts.Wpf; 
-using LiveCharts.WinForms;
-using System.ComponentModel;
 using LiveCharts.Configurations;
-using static WindowsFormsApp4.Form1;
 
 /** SEIZURE FILTER EEG PROGRAM
  * Started by Dominic DiCarlo September 2018
@@ -297,46 +290,5 @@ namespace WindowsFormsApp4
         }
 
 
-        /** Dialog promp implementation from Gideon Mulder in stakcoverflow
-         * https://stackoverflow.com/questions/5427020/prompt-dialog-in-windows-forms
-         */
-
-        public class Prompt : IDisposable
-        {
-            private Form prompt { get; set; }
-            public string Result { get; }
-
-            public Prompt(string text, string caption)
-            {
-                Result = ShowDialog(text, caption);
-            }
-            private string ShowDialog(string text, string caption)
-            {
-                prompt = new Form()
-                {
-                    Width = 500,
-                    Height = 150,
-                    FormBorderStyle = FormBorderStyle.FixedDialog,
-                    Text = caption,
-                    StartPosition = FormStartPosition.CenterScreen,
-                    TopMost = true
-                };
-                Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Dock = DockStyle.Top, TextAlign = ContentAlignment.MiddleCenter };
-                TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
-                Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
-                confirmation.Click += (sender, e) => { prompt.Close(); };
-                prompt.Controls.Add(textBox);
-                prompt.Controls.Add(confirmation);
-                prompt.Controls.Add(textLabel);
-                prompt.AcceptButton = confirmation;
-
-                return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
-            }
-
-            public void Dispose()
-            {
-                prompt.Dispose();
-            }
-        }
     }
 }
---
+++ Prompt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+
+    /** Dialog promp implementation from Gideon Mulder in stakcoverflow
+     * Source: https://stackoverflow.com/questions/5427020/prompt-dialog-in-windows-forms
+     */
+
+    public class Prompt : IDisposable
+    {
+        private Form prompt { get; set; }
+        public string Result { get; }
+        public StreamInputParameters input_params;
+
+        public Prompt(string text, string caption)
+        {
+            input_params = new StreamInputParameters();
+            Result = ShowDialog(text, caption);
+        }
+        private string ShowDialog(string text, string caption)
+        {
+            prompt = new Form()
+            {
+                Width = 300,
+                Height = 300,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen,
+                TopMost = true
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Dock = DockStyle.Top, TextAlign = ContentAlignment.MiddleCenter };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 50, Text = input_params.hostname};
+            Button confirmation = new Button() { Text = "Ok", Left = 100, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        public void Dispose()
+        {
+            prompt.Dispose();
+        }
+    }
+}
